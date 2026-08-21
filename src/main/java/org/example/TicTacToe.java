c491927f996fd8625b3b6a26bfc13c6263dfdb29
Fadhel Almalki <fm4680@gmail.com>
2026-08-21 18:53:17 +0300

    Instantiated preliminary project structure plan

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,7 +1,49 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    static Scanner s = new Scanner(System.in);
+    static Random r = new Random();
+
     static void main() {
 
+    }
+
+    static void startWelcoming(){
+    }
+
+    static int chooseGameRound(){
+        return 0;
+    }
+
+    static int playOneGameRound(){
+        return 0;
+    }
+
+    static void playThreeGameRounds(){
+    }
+
+    static char[][] makeGameBoard(){
+        return null;
+    }
+
+    static void displayGameBoard(){
+    }
+
+    static void makePlayerMove(){
+    }
+
+    static void makeComputerMove(){
+    }
+
+    static boolean checkGameWinner(){
+        return false;
+    }
+
+    static boolean checkIsGameBoardFull(){
+        return false;
     }
 }
